ed070d8cc496d4fb3ddd00e63e6ee442c2db8d78
Wandering King 072 <miguel@samourai.is>
2024-07-12 12:44:23 +0200

    new soroban url

--- java/com/samourai/wallet/dexConfig/SamouraiConfig.java
+++ java/com/samourai/wallet/dexConfig/SamouraiConfig.java
@@ -12,7 +12,7 @@ public class SamouraiConfig {
 
     // extlibj: SorobanServer
     private String sorobanServerTestnetClear = "https://soroban.samouraiwallet.com/test";
-    private String sorobanServerTestnetOnion = "http://vnecqo2arlbgsxezbz2rwkchev2t7waopkymcq47d7qg2tphbddxlsqd.onion";
+    private String sorobanServerTestnetOnion = "http://ashi5mz4i5shis4uzvwc52yw3ewgot3xv6s6lbbfcsskbijffr2h6eqd.onion:4242";
     private String sorobanServerMainnetClear = "https://soroban.samouraiwallet.com"; //TEST URL SHOULD CHANGE FOR ACTUAL SERVER
     private String sorobanServerMainnetOnion = "http://sorob4sg7yiopktgz4eom7hl5mcodr6quvhmdpljl5qqhmt6po7oebid.onion";
 
